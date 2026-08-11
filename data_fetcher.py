42f10369493f31355f2cc0e16e49c7da4f45a67d
Elkin Garcia <elkingarcia.11@gmail.com>
2026-08-10 21:30:52 -0400

    first commit

---
+++ data_fetcher.py
@@ -0,0 +1,58 @@
+"""
+PLayer hitting stats -> expanded (ytd by default )
+https://www.mlb.com/stats/
+last 7 days
+https://www.mlb.com/stats/?timeframe=-6
+last 15 days
+https://www.mlb.com/stats/?timeframe=-14
+last 30 days 
+https://www.mlb.com/stats/?timeframe=-29
+
+ytd home games 
+https://www.mlb.com/stats/?split=h
+ytd away games
+https://www.mlb.com/stats/?split=a
+ytd day games
+https://www.mlb.com/stats/?split=d
+ytd night games
+https://www.mlb.com/stats/?split=n
+on grass
+https://www.mlb.com/stats/?split=g
+on turf
+https://www.mlb.com/stats/?split=t
+pre all star
+https://www.mlb.com/stats/?split=preas
+post all star
+https://www.mlb.com/stats/?split=posas
+vs al 
+https://www.mlb.com/stats/?split=val
+vs nl 
+https://www.mlb.com/stats/?split=vnl
+vs lhp
+https://www.mlb.com/stats/?split=vl
+vs rhp 
+https://www.mlb.com/stats/?split=vr
+on monday
+https://www.mlb.com/stats/?split=dmo
+on tuesday
+https://www.mlb.com/stats/?split=dtu
+on wednesday
+https://www.mlb.com/stats/?split=dwe
+on thursday
+https://www.mlb.com/stats/?split=dth
+on friday
+https://www.mlb.com/stats/?split=dfr
+on saturday
+https://www.mlb.com/stats/?split=dsa
+on sunday
+https://www.mlb.com/stats/?split=dsu
+
+same endpoints for pitching
+https://www.mlb.com/stats/pitching
+
+same ednpoints for team hitting
+https://www.mlb.com/stats/team
+
+same endpoints for team pitching
+https://www.mlb.com/stats/team/pitching
+"""
